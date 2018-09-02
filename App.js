60fb6da52dcf59e61f049c3cc4629b683f5229ae
Thomas Duffy <tomjamesduffy@gmail.com>
2018-09-02 11:52:54 +0100

    Create store and point towards reducers

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import { createStore} from 'redux';
+import { Provider, connect } from 'react-redux';
+import reducer from './reducer';
+
+const store = createStore(reducer)
+// Create redux store
+// Pass in your reducer file as an argument
+// You can also pass in a reducer folder as an argument
+
 
 export default class App extends Component<Props> {
   render() {
     return (
-      
       <View style={styles.container}>
         <Text>Hello World</Text>
       </View>
